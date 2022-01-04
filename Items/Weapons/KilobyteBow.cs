728e3f7588b09675be6941df30a6908b538aa4a5
Oleg Shcherbakov <59744113+FrostHoll@users.noreply.github.com>
2022-01-05 04:47:56 +0500

    pre-v0.1.1-2 -decreased ByteSword knockback -added KilobyteSword and KilobyteProjectile -added KilobyteBow -added localization to new items

---
+++ Items/Weapons/KilobyteBow.cs
@@ -0,0 +1,65 @@
+﻿using Microsoft.Xna.Framework;
+using Terraria;
+using Terraria.DataStructures;
+using Terraria.ID;
+using Terraria.ModLoader;
+using static Terraria.ModLoader.ModContent;
+
+namespace MyTestMod.Items.Weapons
+{
+	public class KilobyteBow : ModItem
+	{
+		public override void SetStaticDefaults()
+		{
+			Tooltip.SetDefault("These sensations are truly scary...\n" +
+				"Has 33% chance not to consume ammo");
+		}
+
+		public override void SetDefaults()
+		{
+
+			Item.damage = 50;
+			Item.DamageType = DamageClass.Ranged;
+			Item.width = 48;
+			Item.height = 48;
+			Item.useTime = 19;
+			Item.useAnimation = 19;
+			//Item.reuseDelay = 10;
+			Item.useStyle = ItemUseStyleID.Shoot;
+			Item.knockBack = 2.5f;
+			Item.value = Item.sellPrice(gold: 1, silver: 60);
+			Item.rare = ItemRarityID.Pink;
+			Item.UseSound = SoundID.Item5;
+			Item.autoReuse = true;
+			Item.useAmmo = AmmoID.Arrow;
+			Item.shoot = ProjectileID.WoodenArrowFriendly;
+			Item.shootSpeed = 11f;
+			Item.scale = 0.8f;
+		}
+
+		public override void AddRecipes()
+		{
+			Recipe recipe = CreateRecipe();
+			recipe.AddIngredient(ItemType<Items.Weapons.ByteBow>(), 1);
+			recipe.AddIngredient(ItemType<Items.KilobyteShard>(), 1);
+			recipe.AddTile(ModContent.TileType<Tiles.TilePC>());
+			recipe.Register();
+		}
+
+        public override bool Shoot(Player player, ProjectileSource_Item_WithAmmo source, Vector2 position, Vector2 velocity, int type, int damage, float knockback)
+        {
+			int numberProjectiles = 3;
+			for (int i = 0; i < numberProjectiles; i++)
+			{
+				Vector2 perturbedSpeed = velocity.RotatedByRandom(MathHelper.ToRadians(15));
+				Projectile.NewProjectile(source, position.X, position.Y, perturbedSpeed.X, perturbedSpeed.Y, type, damage, knockback, player.whoAmI);
+			}
+			return false;
+		}
+
+		public override bool CanConsumeAmmo(Player player)
+		{
+			return Main.rand.NextFloat() >= .33f;
+		}
+	}
+}
--- Items/Weapons/KilobyteSword.cs
+++ Items/Weapons/KilobyteSword.cs
@@ -5,36 +5,37 @@ using static Terraria.ModLoader.ModContent;
 
 namespace MyTestMod.Items.Weapons
 {
-    class ByteSword : ModItem
+    class KilobyteSword : ModItem
     {
         public override void SetStaticDefaults()
         {
-            Tooltip.SetDefault("This is x8 times more powerful sword than Bit Sword! (nope)");
+            Tooltip.SetDefault("You are feeling determination in your hand...");
         }
 
         public override void SetDefaults()
         {
-            Item.damage = 60;
+            Item.damage = 70;
             Item.DamageType = DamageClass.Melee;
-            Item.width = 40;
-            Item.height = 40;
-            Item.useTime = 20;
-            Item.useAnimation = 20;
+            Item.width = 48;
+            Item.height = 48;
+            Item.useTime = 16;
+            Item.useAnimation = 16;
             Item.useStyle = ItemUseStyleID.Swing;
-            Item.knockBack = 3.5f;
+            Item.knockBack = 4.5f;
             Item.scale = 1.2f;
             Item.value = 10000;
-            Item.rare = ItemRarityID.Orange;
+            Item.rare = ItemRarityID.Pink;
             Item.UseSound = SoundID.Item1;
             Item.autoReuse = true;
-            Item.shoot = ModContent.ProjectileType<Projectiles.EightProjectile>();
+            Item.shoot = ModContent.ProjectileType<Projectiles.KilobyteSwordProjectile>();
+            Item.shootSpeed = 10f;
         }
 
         public override void AddRecipes()
         {
             Recipe recipe = CreateRecipe();
-            recipe.AddIngredient(ItemType<BitSword>(), 1);
-            recipe.AddIngredient(ItemType<ByteShard>(), 1);
+            recipe.AddIngredient(ItemType<Items.Weapons.ByteSword>(), 1);
+            recipe.AddIngredient(ItemType<KilobyteShard>(), 1);
             recipe.AddTile(ModContent.TileType<Tiles.TilePC>());
             recipe.Register();
         }
