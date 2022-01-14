ae0fabfcd2c1307ddf359d5dee276f3a1ec44b46
Oleg Shcherbakov <59744113+FrostHoll@users.noreply.github.com>
2022-01-14 20:18:59 +0500

    pre-v0.1.2-1 -added Kilobyte MultiTool

---
+++ Items/Tools/KilobyteMultiToolHamaxe.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+using Terraria.Utilities;
+
+namespace MyTestMod.Items.Tools
+{
+	public class KilobyteMultiToolHamaxe : ModItem
+	{
+		public override void SetStaticDefaults()
+		{
+			DisplayName.SetDefault("Kilobyte MultiTool [Hamaxe]");
+			Tooltip.SetDefault("Right click to switch to pickaxe");
+		}
+
+		public override void SetDefaults()
+		{
+
+			Item.damage = 60;
+			Item.DamageType = DamageClass.Melee;
+			Item.width = 40;
+			Item.height = 40;
+			Item.useTime = 8;
+			Item.useAnimation = 27;
+			Item.useStyle = ItemUseStyleID.Swing;
+			Item.knockBack = 7f;
+			Item.value = 15000;
+			Item.rare = ItemRarityID.Pink;
+			Item.UseSound = SoundID.Item1;
+			Item.axe = 30;
+			Item.hammer = 85;
+			Item.autoReuse = true;
+			Item.useTurn = true;
+		}
+
+		public override bool CanRightClick()
+		{
+			return true;
+		}
+
+		public override void RightClick(Player player)
+		{
+			int _item = player.QuickSpawnItem(ModContent.ItemType<Items.Tools.KilobyteMultiToolPickaxe>());
+		}
+
+		public override bool? PrefixChance(int pre, UnifiedRandom rand) => (pre != -3 && pre != -1);
+	}
+}
---
+++ Items/Tools/KilobyteMultiToolPickaxe.cs
@@ -0,0 +1,58 @@
+﻿using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+using Terraria.Utilities;
+
+namespace MyTestMod.Items.Tools
+{
+	public class KilobyteMultiToolPickaxe : ModItem
+	{
+		public override void SetStaticDefaults()
+		{
+			DisplayName.SetDefault("Kilobyte MultiTool [Pickaxe]");
+			Tooltip.SetDefault("Right click to switch to hamaxe");
+		}
+
+		public override void SetDefaults()
+		{
+			Item.damage = 35;
+			Item.DamageType = DamageClass.Melee;
+			Item.width = 40;
+			Item.height = 40;
+			Item.useTime = 7;
+			Item.useAnimation = 24;
+			Item.useStyle = ItemUseStyleID.Swing;
+			Item.knockBack = 4f;
+			Item.value = 15000;
+			Item.rare = ItemRarityID.Pink;
+			Item.UseSound = SoundID.Item1;
+			Item.pick = 200;
+			Item.autoReuse = true;
+			Item.useTurn = true;
+		}
+
+		public override void AddRecipes()
+		{
+			Recipe recipe = CreateRecipe();
+			recipe.AddIngredient(ModContent.ItemType<Items.Tools.ByteMultiToolPickaxe>(), 1);
+			recipe.AddIngredient(ModContent.ItemType<Items.KilobyteShard>(), 1);
+			recipe.AddIngredient(ItemID.SoulofFright, 1);
+			recipe.AddIngredient(ItemID.SoulofMight, 1);
+			recipe.AddIngredient(ItemID.SoulofSight, 1);
+			recipe.AddTile(ModContent.TileType<Tiles.TilePC>());
+			recipe.Register();
+		}
+
+		public override bool CanRightClick()
+		{
+			return true;
+		}
+
+		public override void RightClick(Player player)
+		{
+			player.QuickSpawnItem(ModContent.ItemType<Items.Tools.KilobyteMultiToolHamaxe>());
+		}
+
+		public override bool? PrefixChance(int pre, UnifiedRandom rand) => (pre != -3 && pre != -1);
+	}
+}
